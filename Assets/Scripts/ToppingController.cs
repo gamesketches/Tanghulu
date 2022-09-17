cacd8956840d69d132b3eb277373dc759e7caa83
Sam Von Ehren <sam.von.ehren@gmail.com>
2022-09-16 22:45:42 -0400

    First commit, most of the basic plumbing is there though the feedback sucks

---
+++ Assets/Scripts/ToppingController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ToppingController : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public ToppingType toppingType;
+    private SpriteRenderer spriteRenderer;
+    Vector3 startPosition;
+    public StickController stick;
+
+    public float distanceToPoke;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        AssignSprite();
+    }
+
+    public void UpdateFruitType(ToppingType newType) {
+        toppingType = newType;
+        AssignSprite();
+    }
+
+    public void ToggleRenderer(bool mode)
+    {
+        spriteRenderer.enabled = mode;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Begin Drag");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector3 newPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        newPos.z = 0;
+        transform.position = newPos;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (Vector3.Distance(transform.position, stick.transform.position) < distanceToPoke)
+            stick.AddFruit(toppingType);
+        transform.position = startPosition;
+    }
+
+    void AssignSprite()
+    {
+        switch (toppingType)
+        {
+            case ToppingType.BLUE:
+                spriteRenderer.color = Color.blue;
+                break;
+            case ToppingType.RED:
+                spriteRenderer.color = Color.red;
+                break;
+            case ToppingType.GREEN:
+                spriteRenderer.color = Color.green;
+                break;
+            case ToppingType.ORANGE:
+                spriteRenderer.color = new Color(1, 0.5f, 0);
+                break;
+            case ToppingType.PURPLE:
+                spriteRenderer.color = Color.magenta;
+                break;
+        }
+    }
+}
---
+++ Assets/StickController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickController : MonoBehaviour
+{
+    public ToppingController[] fruits;
+    int pokedFruit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pokedFruit = 0;
+        foreach (ToppingController fruit in fruits) fruit.ToggleRenderer(false);
+    }
+
+    public void AddFruit(ToppingType newFruit) {
+        fruits[pokedFruit].ToggleRenderer(true);
+        fruits[pokedFruit].UpdateFruitType(newFruit);
+        pokedFruit++;
+        if (pokedFruit >= fruits.Length) GameManager.Instance.VerifyTanghulu();
+    }
+
+    public void ClearStick() { 
+        pokedFruit = 0;
+        foreach (ToppingController fruit in fruits) fruit.ToggleRenderer(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
