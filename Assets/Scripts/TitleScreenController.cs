f36fc1aaa99010637a2bf1034fd087f10f0284ca
Sam Von Ehren <sam.von.ehren@gmail.com>
2022-10-17 22:21:33 -0400

    Adding button to title screen

---
+++ Assets/Scripts/TitleScreenController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame() {
+        SceneManager.LoadScene(1);
+    }
+}
