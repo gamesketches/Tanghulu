e307fb1528041f7a42e8a7f38b37ec2eada35e65
Sam Von Ehren <sam.von.ehren@gmail.com>
2022-10-22 16:18:30 -0400

    Adding in comic panel scene and control flow

---
+++ Assets/Scripts/ComicController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class ComicController : MonoBehaviour
+{
+    public ComicImage[] comicImages;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (ComicImage panel in comicImages) panel.image.enabled = false;
+        StartCoroutine(PlayComic());
+    }
+
+    IEnumerator PlayComic() { 
+        foreach(ComicImage panel in comicImages) {
+            panel.image.enabled = true;
+            yield return new WaitForSeconds(panel.playTime);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+[Serializable]
+public class ComicImage {
+    public Image image;
+    public float playTime;
+}
